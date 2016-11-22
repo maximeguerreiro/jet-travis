447abad7e685ce205083ec845bcc087db18507eb
Maxime Guerreiro <maxime.guerreiro@corp.ovh.com>
2016-11-22 16:07:34 +0100

    :tada:

---
+++ src/main/java/fr/mguerreiro/jetdemo/Main.java
@@ -0,0 +1,11 @@
+package fr.mguerreiro.jetdemo;
+
+public class Main {
+	private Main() {
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hello, World!");
+	}
+}
